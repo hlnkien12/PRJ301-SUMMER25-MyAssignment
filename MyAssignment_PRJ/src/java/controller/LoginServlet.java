db98b9fcacfa853c847b7adbfab0c2870c0ab2d1
hlnkien12 <141345978+hlnkien12@users.noreply.github.com>
2025-07-20 17:27:01 +0700

    Uppdate LoginServlet
    đổi login.html thành login.jsp ở
    request.getRequestDispatcher("/login.jsp").forward(request, response);

--- MyAssignment_PRJ/src/java/controller/LoginServlet.java
+++ MyAssignment_PRJ/src/java/controller/LoginServlet.java
@@ -15,7 +15,7 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        request.getRequestDispatcher("/login.html").forward(request, response);
+        request.getRequestDispatcher("/login.jsp").forward(request, response);
     }
 
     @Override
